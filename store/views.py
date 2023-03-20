382a069fec0ac7d2fc31400bbb9f517c02f7bcb7
mrTchou <dev.mrtchou@gmail.com>
2023-03-20 12:38:34 +0100

    h

--- store/views.py
+++ store/views.py
@@ -4,11 +4,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+
+
+
 def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', context={"products": products})
 
 
+
+
+
+
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
-    return HttpResponse(f"{product.name} {product.price} $")
+    return render(request, 'store/detail.html', context={"product":product})
