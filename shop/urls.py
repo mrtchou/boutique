f60ca5f31fc3dc31ff31976f609ac27846d77f49
mrTchou <dev.mrtchou@gmail.com>
2023-03-20 11:44:57 +0100

    modification class models pour db

--- shop/urls.py
+++ shop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from store.views import index
+from shop import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -14,6 +14,7 @@ Product
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128)
     price = models.FloatField(default=0.0)
     stock = models.IntegerField(default=0)
     description = models.TextField(blank=True)
