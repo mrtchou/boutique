5cf887d7c0da7ad3e29f232f1c6083f9b706401c
mrTchou <dev.mrtchou@gmail.com>
2023-03-20 11:26:09 +0100

    creation views product, urls settings for media

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'store/index.html', context={"products": products})
