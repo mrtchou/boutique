038564d407119ff7e79c5c5e6b7013ce41c56c45
mrTchou <dev.mrtchou@gmail.com>
2023-03-20 23:14:26 +0100

    recuperation info form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, login
+
+User = get_user_model()
 
 # Create your views here.
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username,
+                                 password=password)
+        login(request, user)
+        return redirect('index')
+    
     return render(request, 'accounts/signup.html')
