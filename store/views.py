fe19b2b9ef092a87fe280adc5ae2f1b6e9bfe7e4
mrTchou <dev.mrtchou@gmail.com>
2023-03-20 12:03:16 +0100

    creation views product_detail, creation page404,

--- store/views.py
+++ store/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', context={"products": products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return HttpResponse(f"{product.name} {product.price} $")
