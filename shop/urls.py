de32b970c39c8abd42280ec0368f5152ec1faf4f
mrTchou <dev.mrtchou@gmail.com>
2023-03-21 13:43:05 +0100

    creaton function add to card

--- shop/urls.py
+++ shop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from store.views import index, product_detail
+from store.views import index, product_detail, add_to_card
 from shop import settings
 from django.conf.urls.static import static
 from accounts.views import signup, logout_user, login_user
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('product/<str:slug>', product_detail, name="product"),
+    path('product/<str:slug>/add_to_card', add_to_card, name="add_to_card"),
     path('signup', signup, name="signup"),
     path('logout', logout_user, name="logout"),
     path('login', login_user, name="login"),
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from store.models import Product
+from django.urls import reverse
+from django.shortcuts import redirect, render, get_object_or_404
+from store.models import Product, Card, Order
 from django.http import HttpResponse
 
 # Create your views here.
@@ -20,3 +21,29 @@ def index(request):
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'store/detail.html', context={"product":product})
+
+
+#function pour ajouter produit au panier
+
+def add_to_card(request, slug):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    #ici on recupere deux elements a droites, donc besoin
+    #deux variable a gauche, le _ underscore signifi par 
+    # convention une variable qui ne sera pas utilisé 
+    # par la suite
+    card, _ = Card.objects.get_or_create(user=user)
+    #donc cela verifie si un produit est associé avec cet user, donc on recupere
+    #si non on cré un new card(created)
+    order, created = Order.objects.get_or_create(user=user,
+                                                 product=product)
+    #cas ou n'existais pas, donc créé
+    if created:
+        #donc dans card(panier) on ajoute dans champ orders dans db le order(produit) plus haut l'element qu'on a recuperé avec le bouton ajouter au panier...
+        card.orders.add(order)
+        card.save()
+    else:
+        order.quantity += 1
+        order.save()
+
+    return redirect(reverse("product", kwargs={"slug":slug}))
