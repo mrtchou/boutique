0b7ee0a1ac993f035191f151c1560a59f63f6a08
mrTchou <dev.mrtchou@gmail.com>
2023-03-21 11:47:09 +0100

    creation function connexion(login), url, base.html, login.html

--- shop/urls.py
+++ shop/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from store.views import index, product_detail
 from shop import settings
 from django.conf.urls.static import static
-from accounts.views import signup, logout_user
+from accounts.views import signup, logout_user, login_user
 
 urlpatterns = [
     path('', index, name='index'),
@@ -26,5 +26,6 @@ urlpatterns = [
     path('product/<str:slug>', product_detail, name="product"),
     path('signup', signup, name="signup"),
     path('logout', logout_user, name="logout"),
+    path('login', login_user, name="login"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
